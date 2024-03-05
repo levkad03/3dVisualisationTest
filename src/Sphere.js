292a5578e82d816c9440f63dca29d72528be71e1
rewop <dyalist2001@gmail.com>
2024-03-05 18:45:27 +0200

    added 3d 4elove4ek's model from radiation pattern example

--- src/Sphere.js
+++ src/Sphere.js
@@ -1,6 +1,28 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+const changePointPosition = (points, wSegments, segment, pointIndex, newPosition) => {
+  if (pointIndex == 0){
+    points[(segment*(wSegments+1)+wSegments)*3] += newPosition[0];
+    points[(segment*(wSegments+1)+wSegments)*3 + 1] += newPosition[1];
+    points[(segment*(wSegments+1)+wSegments)*3 + 2] += newPosition[2];
+  }
+
+  points[(segment*(wSegments+1)+pointIndex)*3] += newPosition[0];
+  points[(segment*(wSegments+1)+pointIndex)*3 + 1] += newPosition[1];
+  points[(segment*(wSegments+1)+pointIndex)*3 + 2] += newPosition[2];
+
+}
+
+const polarToCartesian = (radius, angleDegrees) => {
+  var angleRadians = angleDegrees * Math.PI / 180;
+  var x = radius * Math.cos(angleRadians);
+  var y = radius * Math.sin(angleRadians);
+  return [x, y];
+}
+
+
+
 export const createLightRayScene = () => {
   // Создание сцены
   const scene = new THREE.Scene();
@@ -12,22 +34,24 @@ export const createLightRayScene = () => {
     0.1,
     1000
   );
-  camera.position.z = 5;
+  camera.position.z = 75;
 
   // Создание рендерера
   const renderer = new THREE.WebGLRenderer({ alpha: false });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const axis = new THREE.AxesHelper(5);
+  const axis = new THREE.AxesHelper(50);
 
   const tableRightSide = new Float32Array([10, 9.9, 8, 7, 4, 2, -2, -6, -10, -13, -8, -6, -6.5, -8, -10, -7, -3.5, -1.5, -1]);
   const tableLeftSide = new Float32Array([10, 9.9, 7.8, 6, 3, 0, -4, -7.5, -10, -10, -7, -5, -5, -7, -9, -8, -4, -2, -1]);
   const tableMean = new Float32Array([10, 9.9, 7.9, 6.5, 3.5, 1, -3, -6.75, -10, -11.5, -7.5, -5.5, -5.75, -7.5, -9.5, -7.5, -3.75, -1.75, -1]);
-  const tableMeanCopy = tableMean;
 
   // Создание BufferGeometry для сферы
-  const sphereGeometry = new THREE.SphereGeometry(2, 16, 16);
+  const radius = 20;
+  const widthSegments = 4;
+  const heightSegments = 18;
+  const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 
   // getting number of points
   const count = sphereGeometry.attributes.position.count;
@@ -44,7 +68,7 @@ export const createLightRayScene = () => {
 
   // setting color
   for (let i = 0; i < count; i++) {
-    color.setRGB(1, 0.8 - (positions.getY(i) / 2 + 1) / 2, 0);
+    color.setRGB(1, 0.8 - (positions.getY(i) / radius + 1) / 2, 0);
     sphereColors.setXYZ(i, color.r, color.g, color.b);
   }
 
@@ -78,36 +102,51 @@ export const createLightRayScene = () => {
     console.log(`Point ${i / 3}: x=${x}, y=${y}, z=${z}`);
   }
 
-  const offsetVector = new THREE.Vector3(0, 2, 0);
+  const offsetVector = new THREE.Vector3(2, 0, 0);
 
   // Изменение расстояния точек от центра с использованием вектора
-  for (let index = 0; index < 16; index++) {
-    const vertex = new THREE.Vector3().fromArray(positionsOffset, index * 3);
-    vertex.add(offsetVector);
-    vertex.toArray(positionsOffset, index * 3);
+  // for (let index = Math.ceil(segments/2)*5;index < (Math.ceil(segments/2) + 1)*5; index++) {
+  //   const vertex = new THREE.Vector3().fromArray(positionsOffset, index * 3);
+  //   vertex.add(offsetVector);
+  //   vertex.toArray(positionsOffset, index * 3);
+  // }
+  
+  for (let index = 0; index < tableRightSide.length; index++) {
+    const startPosition = polarToCartesian(radius, 10*index + 90);
+    const endPosition = polarToCartesian(radius + tableRightSide[index], 10*index + 90);
+    const deltaX = endPosition[0] - startPosition[0];
+    const deltaY = endPosition[1] - startPosition[1];
+    console.log(`Point x=${deltaX}, y=${deltaY}, index=${index}`)
+    changePointPosition(positionsOffset, widthSegments, index, 2, [-deltaX, deltaY, 0]);
   }
 
-  // Укажите Three.js, что буфер атрибутов был изменен
-  sphereGeometry.getAttribute("position").needsUpdate = true;
+  for (let index = 0; index < tableLeftSide.length; index++) {
+    const startPosition = polarToCartesian(radius, 10*index + 90);
+    const endPosition = polarToCartesian(radius + tableLeftSide[index], 10*index + 90);
+    const deltaX = endPosition[0] - startPosition[0];
+    const deltaY = endPosition[1] - startPosition[1];
+    console.log(`Point x=${deltaX}, y=${deltaY}, index=${index}`)
+    changePointPosition(positionsOffset, widthSegments, index, 0, [deltaX, deltaY, 0]);
+  }
 
-  // changing position of points [Sphere]
-  // for (let index = 0; index < 16; index++) {
-  //   positionsOffset[index*3] += 2;
-  //   positionsOffset[index*3 + 1] += 2;
-  //   positionsOffset[index*3 + 2] += 2;
-  // }
+  for (let index = 0; index < tableMean.length; index++) {
+    const startPosition = polarToCartesian(radius, 10*index + 90);
+    const endPosition = polarToCartesian(radius + tableMean[index], 10*index + 90);
+    const deltaX = endPosition[0] - startPosition[0];
+    const deltaY = endPosition[1] - startPosition[1];
+    console.log(`Point x=${deltaX}, y=${deltaY}, index=${index}`)
+    changePointPosition(positionsOffset, widthSegments, index, 1, [0, deltaY, -deltaX]);
+  }
 
-  // for (let index = 16; index < 34; index++) {
-  //   positionsOffset[index*3 + 1] = 3;
-  // }
+  for (let index = 0; index < tableMean.length; index++) {
+    const startPosition = polarToCartesian(radius, 10*index + 90);
+    const endPosition = polarToCartesian(radius + tableMean[index], 10*index + 90);
+    const deltaX = endPosition[0] - startPosition[0];
+    const deltaY = endPosition[1] - startPosition[1];
+    console.log(`Point x=${deltaX}, y=${deltaY}, index=${index}`)
+    changePointPosition(positionsOffset, widthSegments, index, 3, [0, deltaY, deltaX]);
+  }
 
-  // for (let index = 34; index < 51; index++) {
-  //   positionsOffset[index*3 + 1] = 4;
-  // }
-
-  // for (let index = 51; index < 68; index++) {
-  //   positionsOffset[index*3 + 1] = 5;
-  // }
 
   // Добавление меша на сцену
   scene.add(axis);
