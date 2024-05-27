55427b39fd969effb15e066e31f82bccb8565563
heroband <dyalist2001@gmail.com>
2024-05-27 13:28:59 +0300

    added different input tables processing

--- src/Sphere.js
+++ src/Sphere.js
@@ -480,12 +480,37 @@ const scaleGeometry = (geometry, scaleFactor) => {
 
 }
 
-const createTable = (table, length) => {
-  let resultTable = new Float32Array(length);
-  for (let i = 0; i < length; i++) {
-    resultTable[i] = i < table.length ? table[i] : 0;
+const initTables = (tables) => {
+  let length = 0;
+  let minNumber = 0;
+
+  let inlineTable = [];
+
+  for (let table in tables) {
+    inlineTable.push(...tables[table]);
   }
-  return resultTable;
+
+
+  minNumber = Math.min(0, ...inlineTable); 
+
+  for (let i = 0; i < tables.length; i++) {
+    if (length < tables[i].length){
+      length = tables[i].length;
+    }
+  }
+
+  
+  let resultTables = [];
+  for (let i = 0; i < tables.length; i++) {
+    resultTables[i] = new Float32Array(length);
+    for (let j = 0; j < length; j++) {
+      resultTables[i][j] = j < tables[i].length ? tables[i][j] : minNumber;
+    }
+    console.log(resultTables[i]);
+  }
+
+
+  return resultTables;
 }
 
 export const createLightRayScene = () => {
@@ -529,45 +554,24 @@ export const createLightRayScene = () => {
 
   switch (model_type) {
     case "different":
-      // tableRightSide = createTable(new Float32Array([
-      //   0, -0.3, -0.4, -1.5, -1.7, -2, -2.5, -2.6, -3.4, -5, -6, -8, -9, -10.8,
-      //   -12.6, -14.5, -16.7, -20, -24.5, -30, -33, -25, -28, -24.5, -22.5, -24.5,
-      //   -23.3, -25, -26.3, -29, -33.4, -23, -23, -22.3, -25, -32, -33,
-      // ]), tableLength)
-
       tableRightSide = new Float32Array([
         0, -0.3, -0.4, -1.5, -1.7, -2, -2.5, -2.6, -3.4, -5, -6, -8, -9, -10.8,
         -12.6, -14.5, -16.7, -20, -24.5, -30, -33, -25, -28, -24.5, -22.5, -24.5,
         -23.3, -25, -26.3, -29, -33.4, -23, -23, -22.3, -25, -32, -33,
-      ]);
+      ]); // 
 
-      // tableLeftSide = createTable(new Float32Array([
-      //   0, -0.05, -0.2, -0.7, -1.7, -2.5, -3.3, -3.5, -5, -5.4, -5.5, -6, -7.5,
-      //   -8.33, -9.5, -11, -11.7, -12, -15, -16.7, -16.7, -20, -19, -16.7, -18.3,
-      //   -23.3, -30, -23.8, -20, -24, -23.3, -32.5, -27.5, -28.5, -33.5, -31, -33,
-      // ]), tableLength)
       tableLeftSide = new Float32Array([
         0, -0.05, -0.2, -0.7, -1.7, -2.5, -3.3, -3.5, -5, -5.4, -5.5, -6, -7.5,
         -8.33, -9.5, -11, -11.7, -12, -15, -16.7, -16.7, -20, -19, -16.7, -18.3,
         -23.3, -30, -23.8, -20, -24, -23.3, -32.5, -27.5, -28.5, -33.5, -31, -33,
       ]);
 
-      // tableTop = createTable(new Float32Array([
-      //   0, -0.05, -0.8, -2.5, -5, -7.6, -11.3, -17, -26, -23, -22, -22, -21.5,
-      //   -22.5, -22, -25, -24.5, -23.5, -24, -22.5, -23, -22, -24, -26, -22, -25,
-      //   -29, -34, -28.4, -28.4, -27.5, -31, -26, -24, -20, -22, -26,
-      // ]), tableLength)
       tableTop = new Float32Array([
         0, -0.05, -0.8, -2.5, -5, -7.6, -11.3, -17, -26, -23, -22, -22, -21.5,
         -22.5, -22, -25, -24.5, -23.5, -24, -22.5, -23, -22, -24, -26, -22, -25,
         -29, -34, -28.4, -28.4, -27.5, -31, -26, -24, -20, -22, -26,
       ]);
 
-      // tableBottom = createTable(new Float32Array([
-      //   0, -0.8, -2, -4, -6.7, -10, -14.5, -20, -21.6, -24, -24, -23.3, -24, -23,
-      //   -23.5, -23.6, -30, -30.8, -34.5, -32.5, -27.5, -28.3, -31.3, -33, -30, -26,
-      //   -25, -22.5, -21.6, -25, -28, -27, -31, -29, -26.7, -26, -26,
-      // ]), tableLength)
       tableBottom = new Float32Array([
         0, -0.8, -2, -4, -6.7, -10, -14.5, -20, -21.6, -24, -24, -23.3, -24, -23,
         -23.5, -23.6, -30, -30.8, -34.5, -32.5, -27.5, -28.3, -31.3, -33, -30, -26,
@@ -575,39 +579,36 @@ export const createLightRayScene = () => {
       ]);
 
       data = [tableRightSide, tableTop, tableLeftSide, tableBottom];
+      data = initTables(data);
       break;
     case "partially_similar":
-      // tableRightSide = createTable(new Float32Array(["some_data_from_input_1"]), tableLength)
       tableRightSide = new Float32Array([
         0, -0.3, -0.4, -1.5, -1.7, -2, -2.5, -2.6, -3.4, -5, -6, -8, -9, -10.8,
         -12.6, -14.5, -16.7, -20, -24.5, -30, -33, -25, -28, -24.5, -22.5, -24.5,
         -23.3, -25, -26.3, -29, -33.4, -23, -23, -22.3, -25, -32, -33,
       ]);
 
-      // tableLeftSide = createTable(new Float32Array(["some_data_from_input_2"]), tableLength)
       tableLeftSide = new Float32Array([
         0, -0.05, -0.2, -0.7, -1.7, -2.5, -3.3, -3.5, -5, -5.4, -5.5, -6, -7.5,
         -8.33, -9.5, -11, -11.7, -12, -15, -16.7, -16.7, -20, -19, -16.7, -18.3,
         -23.3, -30, -23.8, -20, -24, -23.3, -32.5, -27.5, -28.5, -33.5, -31, -33,
       ]);
       data = [tableRightSide, tableLeftSide];
+      data = initTables(data);
       break;
     case "similar":
-      // tableRightSide = createTable(new Float32Array(["some_data_from_input"]), tableLength)
       tableRightSide = new Float32Array([
         0, -0.3, -0.4, -1.5, -1.7, -2, -2.5, -2.6, -3.4, -5, -6, -8, -9, -10.8,
         -12.6, -14.5, -16.7, -20, -24.5, -30, -33, -25, -28, -24.5, -22.5, -24.5,
         -23.3, -25, -26.3, -29, -33.4, -23, -23, -22.3, -25, -32, -33,
       ]);
       data = [tableRightSide];
+      data = initTables(data);
       break;
     default:
       console.error(`Unknown model type: ${model_type}`)
       break;
   }
-
-  
-
 
   if (["different", "partially_similar", "similar"].includes(model_type)){
     // Входные данные - not required
